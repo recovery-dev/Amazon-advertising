318dae2607180be51f0f405e2ad558b4169e0a9e
best service <apple930108@gmail.com>
2018-06-05 02:03:32 +0800

    delete

--- config/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host: "clabdevelopment2.cui8xcfo9xar.us-east-1.rds.amazonaws.com",
-    user: "clabDeveloper",
-    password: "Br3w3ry!",
-    database: "clabDevelopment"
-};
