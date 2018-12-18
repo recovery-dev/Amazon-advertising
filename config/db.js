3f4b655516b13eae1ca67faf07df55e6c02500a0
FullStack <34084225+BeautifulCodeStar@users.noreply.github.com>
2018-12-18 02:43:23 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: "clabdevelopment2.cui8xcfo9xar.us-east-1.rds.amazonaws.com",
-    user: "clabDeveloper",
-    password: "Br3w3ry!",
-    database: "clabDevelopment"
+    host: "",
+    user: "",
+    password: "",
+    database: ""
 };
